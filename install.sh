49b8a2d8001b46ca623172e4edacd9b245658ab0
itxx00 <itxx00@gmail.com>
2013-08-03 10:41:01 +0800

    use screen to run install script

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 ###
+SCREEN_NAME="lanmp"
+# screen_it service "command-line"
+function screen_it {
+    screen -S $SCREEN_NAME -X screen -t $1
+    sleep 1.5
+    screen -S $SCREEN_NAME -p $1 -X stuff "$2"
+}
 if [ $UID != 0 ]; then
     echo "You must be root to run the install script."
     exit
@@ -19,8 +26,22 @@ if [ "$1" == "un" -o "$1" == "uninstall" ]; then
     reboot
     exit
 fi
-
+if type -p screen >/dev/null && screen -ls |grep -q "[0-9].$SCREEN_NAME"; then
+    echo "To rejoin this session type 'screen -x $SCREEN_NAME'."
+    echo "To destroy this session, type './lanmp.sh un'."
+    exit 1
+fi
+if grep -qi ubuntu /etc/issue; then
+    type -p screen >/dev/null || apt-get -y install screen
+else
+    type -p screen >/dev/null || yum -y install screen
+fi
+screen -d -m -S $SCREEN_NAME -t lanmpins -s /bin/bash
+sleep 1.5
 chmod 755 lanmp.sh
 chmod 755 wdcp.sh
-./lanmp.sh | tee lanmp_ins.log
-./wdcp.sh | tee wdcp_ins.log
+#./lanmp.sh | tee lanmp_ins.log
+#./wdcp.sh | tee wdcp_ins.log
+screen_it lanmpins "(./lanmp.sh|tee lanmp_ins.log);(./wdcp.sh|tee wdcp_ins.log)"
+echo "enter screen session"
+screen -r $SCREEN_NAME
