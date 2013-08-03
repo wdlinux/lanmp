c8ca432f7bcaac6fbd2d3f0c2060ae6da20189f6
itxx00 <itxx00@gmail.com>
2013-08-02 20:34:16 -0700

    use screen to manage the install session

--- install.sh
+++ install.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 ###
 SCREEN_NAME="lanmp"
-# screen_it service "command-line"
-function screen_it {
-    screen -S $SCREEN_NAME -X screen -t $1
-    sleep 1.5
-    screen -S $SCREEN_NAME -p $1 -X stuff "$2"
-}
 if [ $UID != 0 ]; then
     echo "You must be root to run the install script."
     exit
@@ -20,28 +14,35 @@ if [ "$1" == "un" -o "$1" == "uninstall" ]; then
     service wdapache stop
     mkdir /www/backup
     bf=$(date +%Y%m%d)
-    tar zcvf /www/backup/mysqlbk_$bf.tar.gz /www/wdlinux/mysql/var
+    tar zcf /www/backup/mysqlbk_$bf.tar.gz /www/wdlinux/mysql/var
     rm -fr /www/wdlinux
     rm -f /tmp/*_ins.txt
     reboot
     exit
 fi
+
 if type -p screen >/dev/null && screen -ls |grep -q "[0-9].$SCREEN_NAME"; then
-    echo "To rejoin this session type 'screen -x $SCREEN_NAME'."
-    echo "To destroy this session, type './lanmp.sh un'."
+    echo "Seems another lanmp install session is taken place."
+    echo "Rejoin this session plz type: 'screen -r $SCREEN_NAME'."
     exit 1
 fi
-if grep -qi ubuntu /etc/issue; then
+
+if grep -qi 'debian\|ubuntu' /etc/issue; then
     type -p screen >/dev/null || apt-get -y install screen
 else
     type -p screen >/dev/null || yum -y install screen
 fi
-screen -d -m -S $SCREEN_NAME -t lanmpins -s /bin/bash
+# prepare screen session for install
+screen -d -m -S $SCREEN_NAME -t lanmp -s /bin/bash
 sleep 1.5
+if [ -z "$SCREEN_HARDSTATUS" ]; then
+    SCREEN_HARDSTATUS='%{= .} %-Lw%{= .}%> %n%f %t*%{= .}%+Lw%< %-=%{g}(%{d}%H/%l%{g})'
+fi
+screen -r $SCREEN_NAME -X hardstatus alwayslastline "$SCREEN_HARDSTATUS"
+NL=$(echo -ne '\015')
 chmod 755 lanmp.sh
 chmod 755 wdcp.sh
-#./lanmp.sh | tee lanmp_ins.log
-#./wdcp.sh | tee wdcp_ins.log
-screen_it lanmpins "(./lanmp.sh|tee lanmp_ins.log);(./wdcp.sh|tee wdcp_ins.log)"
+screen -S $SCREEN_NAME -p lanmp -X stuff \
+    "(./lanmp.sh|tee lanmp_ins.log);(./wdcp.sh|tee wdcp_ins.log)$NL"
 echo "enter screen session"
 screen -r $SCREEN_NAME
