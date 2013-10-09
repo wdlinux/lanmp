adc2450bcf8b99de6e9116e958697c766d8d1c5f
itxx00 <itxx00@gmail.com>
2013-10-09 20:28:00 +0800

    update downloadpkg.sh

--- utils/downloadpkg.sh
+++ utils/downloadpkg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . lib/common.conf
 cd lanmp
-for s in "$HTTPD_DU $NGINX_DU $MYSQL_DU $PHP53_DU $PUREFTP_DU"; do
+for s in "$HTTPD_DU $NGINX_DU $TENGINE_DU $MYSQL_DU $PHP_DU $PHP53_DU $PUREFTP_DU $EACCELERATOR_DU $PHP_FPM_DU $PCRE_DU $LIBICONV_DU"; do
     wget -nc $s
 done
