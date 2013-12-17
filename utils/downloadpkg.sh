80e9887d38a72d4a6017d3d829ae850375abbbb8
xingxing <itxx00@gmail.com>
2013-12-17 11:23:14 +0800

    downloadpkg.sh: change wget arg to -c

--- utils/downloadpkg.sh
+++ utils/downloadpkg.sh
@@ -2,5 +2,5 @@
 . lib/common.conf
 cd lanmp
 for s in "$HTTPD_DU $NGINX_DU $TENGINE_DU $MYSQL_DU $PHP_DU $PHP53_DU $PUREFTP_DU $EACCELERATOR_DU $PHP_FPM_DU $PCRE_DU $LIBICONV_DU"; do
-    wget -nc $s
+    wget -c $s
 done
